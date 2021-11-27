de9f80c803d26571744ac8ed6bf437076f3eef01
Smartman Apps <20635275+SmartmanApps@users.noreply.github.com>
2021-11-27 16:04:45 +1100

    Add project files.

---
+++ MyMauiApp/App.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Maui.Controls;
+using System;
+
+namespace MyMauiApp {public class App :Application {
+public App() {
+    AppDomain.CurrentDomain.FirstChanceException+=CurrentDomain_FirstChanceException;
+    MainPage=new MainPage();
+    }
+
+private void CurrentDomain_FirstChanceException(object sender,System.Runtime.ExceptionServices.FirstChanceExceptionEventArgs e)
+{
+System.Diagnostics.Debug.WriteLine($"********************************** UNHANDLED EXCEPTION! Details: {e.Exception.ToString()}");
+}
+
+protected override void OnStart()
+{
+base.OnStart();
+}
+
+protected override void OnSleep()
+{
+base.OnSleep();
+}
+
+protected override void OnResume()
+{
+base.OnResume();
+}
+
+}}
---
+++ MyMauiApp/MainPage.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Maui.Controls;
+using Microsoft.Maui.Graphics;
+
+namespace MyMauiApp {public class MainPage :ContentPage {
+public MainPage() {
+    Title="MAUI minus XAML";
+    BackgroundColor=Color.FromHex("000000");
+    Label TopLabel=new Label{
+        HorizontalTextAlignment=Microsoft.Maui.TextAlignment.Center,
+        VerticalTextAlignment=Microsoft.Maui.TextAlignment.Center,
+        };
+    TopLabel.TextColor=Color.FromHex("009933");
+    TopLabel.Text="Welcome to MAUI minus XAML!";
+    Content=TopLabel;
+    }
+
+}}
